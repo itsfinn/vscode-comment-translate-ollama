0f3046dd7f1eda95bbca59050ffe81b92d292fae
itsfinn <itsfinn@qq.com>
2024-12-20 13:58:50 +0800

    feat: use ollama

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import { ITranslateRegistry } from 'comment-translate-manager';
 import * as vscode from 'vscode';
-import { ChatGPTTranslate } from './chatgptTranslate';
+import { OllamaTranslate } from './ollamaTranslate';
 
 export function activate(context: vscode.ExtensionContext) {
 	
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 	//Expose the plug-in
 	return {
         extendTranslate: function (registry: ITranslateRegistry) {
-            registry('chatgpt', ChatGPTTranslate);
+            registry('ollama', OllamaTranslate);
         }
     };
 }
--- src/ollamaTranslate.ts
+++ src/ollamaTranslate.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { workspace } from 'vscode';
 import { ITranslate, ITranslateOptions } from 'comment-translate-manager';
 
-const PREFIXCONFIG = 'chatgptTranslate';
+const PREFIXCONFIG = 'ollamaTranslate';
 
 const langMaps: Map<string, string> = new Map([
     ['zh-CN', 'ZH'],
@@ -25,17 +25,18 @@ export function getConfig<T>(key: string): T | undefined {
 
 
 
-interface ChatGPTTranslateOption {
-    authKey?: string;
+interface OllamaTranslateOption {
 }
 
-export class ChatGPTTranslate implements ITranslate {
+export class OllamaTranslate implements ITranslate {
     get maxLen(): number {
+        console.log("maxLen")
         return 3000;
     }
 
-    private _defaultOption: ChatGPTTranslateOption;
+    private _defaultOption: OllamaTranslateOption;
     constructor() {
+        console.log("constructor")
         this._defaultOption = this.createOption();
         workspace.onDidChangeConfiguration(async eventNames => {
             if (eventNames.affectsConfiguration(PREFIXCONFIG)) {
@@ -45,24 +46,22 @@ export class ChatGPTTranslate implements ITranslate {
     }
 
     createOption() {
-        const defaultOption:ChatGPTTranslateOption = {
-            authKey: getConfig<string>('authKey'),
-        };
+        console.log("createOption")
+        const defaultOption:OllamaTranslateOption = {        };
         return defaultOption;
     }
 
     async translate(content: string, { to = 'auto' }: ITranslateOptions) {
 
+        console.log("translate start")
         const url = `http://localhost:11434/api/chat`;
 
-        if(!this._defaultOption.authKey) {
-            throw new Error('Please check the configuration of authKey!');
-        }
         let systemPrompt = "你是一个程序代码注释翻译引擎，请在保留原始段落格式的情况下翻译文本，注意识别并保留段落中的专有名词，缩写和术语以及特定的概念或表达";
         let userPrompt = `将以下英文翻译为简体中文`;
         userPrompt = `${userPrompt}:\n\n"${content}" =>`;
         const body = {
-            // model: 'gpt-3.5-turbo',
+            stream: false,
+            model: "llama3.1",
             temperature: 0,
             max_tokens: 1000,
             top_p: 1,
@@ -79,14 +78,16 @@ export class ChatGPTTranslate implements ITranslate {
         
         const headers = {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${this._defaultOption.authKey}`,
         };
 
         let res = await axios.post(url,body,{
             headers
         });
-        const { choices } = res.data;
-        let targetTxt = choices[0].message.content.trim();
+        console.log("res: ", res)
+        // const { choices } = res.data;
+        // console.log("res.data: ", res.data)
+        let targetTxt = res.data.message.content.trim();
+        console.log("targetTxt: ", targetTxt)
         if (targetTxt.startsWith('"') || targetTxt.startsWith("「")) {
             targetTxt = targetTxt.slice(1);
         }
@@ -98,11 +99,13 @@ export class ChatGPTTranslate implements ITranslate {
 
 
     link(content: string, { to = 'auto' }: ITranslateOptions) {
+        console.log("link")
         let str = `http://localhost:11434/api/chat`;
         return `[ChatGPT](${str})`;
     }
 
     isSupported(src: string) {
+        console.log("isSupported")
         return true;
     }
 }
